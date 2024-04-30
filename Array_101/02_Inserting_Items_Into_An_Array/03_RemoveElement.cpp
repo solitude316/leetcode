b0b4830c2506314ce79f52a5f2f96389bb1071e0
Moin Teng <solitude316@gmail.com>
2024-05-01 01:25:03 +0800

    Complete Remove Element

---
+++ Array_101/02_Inserting_Items_Into_An_Array/03_RemoveElement.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printArr(vector<int>& nums)
+{
+    cout << "(";
+    vector<int>::iterator it;
+
+    for (it = nums.begin(); it != nums.end(); it++) {
+        cout << *it;
+        if (it != nums.end() - 1) {
+            cout << ", ";
+        }
+    }
+
+    cout << ")\n";
+}
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int result = 0;
+    
+        for(int i = 0; i < nums.size(); i++) {
+            if (nums[i] == val) {
+                nums[i] = nums[nums.size() - 1];
+                nums.erase(nums.end() - 1); 
+                result++;
+                i--;
+            }
+            printArr(nums);
+        }
+
+        return result;
+    }
+};
+
+int main(int argc, char** argv) {
+    Solution solution;
+
+    vector<int> nums1 = { 0,1,2,2,3,0,4,2 };
+
+    int result = solution.removeElement(nums1, 2);
+
+    printArr(nums1);
+
+    cout << "result = " << result << endl;
+
+    return 0;
+}
