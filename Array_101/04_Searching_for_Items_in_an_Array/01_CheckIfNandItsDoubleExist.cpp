54f762b4c8ccb9ecafac11e495b2906b62c95fdb
Moin Teng <solitude316@gmail.com>
2024-05-01 13:44:53 +0800

    Finished Check If N and Its Double Exist

---
+++ Array_101/04_Searching_for_Items_in_an_Array/01_CheckIfNandItsDoubleExist.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printArr(vector<int>& nums)
+{
+    cout << "(";
+    vector<int>::iterator it;
+
+    for (it = nums.begin(); it != nums.end(); it++) {
+        cout << *it;
+        if (it != nums.end() - 1) {
+            cout << ", ";
+        }
+    }
+
+    cout << ")\n";
+}
+
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        for(int i = 0; i < arr.size(); i++) {
+            for(int j = i + 1; j < arr.size(); j++) {
+                if ((arr[i] ==  2 * arr[j]) || (2 * arr[i] == arr[j])) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
+
+int main(int argc, char** argv) {
+    Solution solution;
+    vector<int> nums1 = { 10,2,5,3 };
+    // nums1 = {3,1,7,11};
+    nums1 = {7,1,14,11};
+    bool result = solution.checkIfExist(nums1);
+    printArr(nums1);
+    cout << result;
+
+    return 0;
+}
